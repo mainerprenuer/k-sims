6df7ffb07eae87d2311b87e4c11de391fb67e238
Mousty Mainer <moustymainer@gmail.com>
2023-12-28 16:12:47 +0100

    added sponsor section...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -120,7 +120,67 @@ export default function CallToAction() {
           </Box>
         </Flex>
       </Stack>
+
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+        direction={{ base: 'column', md: 'row' }}>
+        <Stack flex={2} spacing={{ base: 5, md: 10 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'green.400',
+                zIndex: -1,
+              }}>
+              Our Sponsor
+            </Text>
+          </Heading>
+          
+          <Stack spacing={{ base: 4, sm: 6 }} direction={{ base: 'column', sm: 'row' }}>
+            <Text as={'span'} color={'white.400'}>
+                this is our sponsors' note
+            </Text>
+          </Stack>
+        </Stack>
+        <Flex
+          flex={1}
+          justify={'center'}
+          align={'center'}
+          position={'relative'}
+          w={'full'}>
+          <Box
+            position={'relative'}
+            height={'300px'}
+            rounded={'2xl'}
+            boxShadow={'2xl'}
+            width={'full'}
+            overflow={'hidden'}>
+           
+            <Image
+              alt={'Hero Image'}
+              fit={'cover'}
+              align={'center'}
+              w={'150%'}
+              h={'150%'}
+              src="/com-dig.jpg"
+            />
+          </Box>
+        </Flex>
+      </Stack>
     </Container>
+    
   )
 }
 
